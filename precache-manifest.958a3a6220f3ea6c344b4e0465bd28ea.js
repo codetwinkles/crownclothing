fb69fe11d92546079593c0fcf691d80dbcf09ab2
Ho Kar Ling <8458817+karlingcodes@users.noreply.github.com>
2019-10-20 07:02:27 +0800

    Updates

---
+++ precache-manifest.958a3a6220f3ea6c344b4e0465bd28ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8796494946640ca460121036599e0bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3241f3cfaf6a5f43f9fc",
+    "url": "/static/css/main.2ab16f4b.chunk.css"
+  },
+  {
+    "revision": "55a8c7ef9a678cb3a3c5",
+    "url": "/static/js/2.41aa64af.chunk.js"
+  },
+  {
+    "revision": "3241f3cfaf6a5f43f9fc",
+    "url": "/static/js/main.275effb6.chunk.js"
+  },
+  {
+    "revision": "2dab1c5a03ab70e11aea",
+    "url": "/static/js/runtime-main.d864f8ec.js"
+  }
+]);
